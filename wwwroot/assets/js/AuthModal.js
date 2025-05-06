d6294e099561297b613db21ff7bf887f3dd842a5
BuniyadByte <155827790+BuniyadByte@users.noreply.github.com>
2025-05-06 15:53:00 +0530

    Add files via upload

---
+++ wwwroot/assets/js/AuthModal.js
@@ -0,0 +1,48 @@
+﻿(function () {
+    'use strict'
+
+    const authModal = document.getElementById('authModal');
+
+    // Confirm password validation
+    //const registerForm = authModal.querySelector('form[action="/Account/Register"]');
+    //const passwordInput = registerForm.querySelector('#registerPassword');
+    //const confirmPasswordInput = registerForm.querySelector('#registerConfirmPassword');
+    //const confirmPasswordFeedback = registerForm.querySelector('#confirmPasswordFeedback');
+
+    //confirmPasswordInput.addEventListener('input', () => {
+    //    if (confirmPasswordInput.value === passwordInput.value) {
+    //        confirmPasswordInput.setCustomValidity('');
+    //        confirmPasswordFeedback.style.display = 'none';
+    //    } else {
+    //        confirmPasswordInput.setCustomValidity('Passwords do not match.');
+    //        confirmPasswordFeedback.style.display = 'block';
+    //    }
+    //});
+
+    // Show register tab from link
+    const showRegisterLink = authModal.querySelector('#showRegisterLink');
+    showRegisterLink.addEventListener('click', (e) => {
+        e.preventDefault();
+        const registerTab = new bootstrap.Tab(document.getElementById('register-tab'));
+        registerTab.show();
+    });
+
+    // Show login tab from link
+    const showLoginLink = authModal.querySelector('#showLoginLink');
+    showLoginLink.addEventListener('click', (e) => {
+        e.preventDefault();
+        const loginTab = new bootstrap.Tab(document.getElementById('login-tab'));
+        loginTab.show();
+    });
+
+    // Enable Bootstrap validation styles on submit
+    authModal.querySelectorAll('form').forEach(form => {
+        form.addEventListener('submit', event => {
+            if (!form.checkValidity()) {
+                event.preventDefault();
+                event.stopPropagation();
+            }
+            form.classList.add('was-validated');
+        }, false);
+    });
+}) ();
